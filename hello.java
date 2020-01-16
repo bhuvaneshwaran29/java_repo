807e1e1bcc7e6ff7c0c5b8faa92a7bcda47a884a
xyz <ubuntu@ip-172-31-43-153.us-east-2.compute.internal>
2020-01-16 10:15:30 +0000

    file added

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello
+{
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+
